13a71e6865fe93179a893159baef3d5c4ecc7d74
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-14 20:30:05 +0900

    feat: add PlayerState as global singleton

---
+++ Scenes/autoload/player_state.gd
@@ -0,0 +1,23 @@
+extends Node
+
+signal health_changed(health: int)
+signal max_health_changed(max_health: int)
+
+@export var max_health := 20
+
+var player_health := max_health
+
+
+func update_max_health(amount: int) -> void:
+	max_health += amount
+	max_health_changed.emit(max_health)
+
+
+func increase_health(amount: int) -> void:
+	player_health = maxi(player_health + amount, max_health)
+	health_changed.emit(player_health)
+
+
+func decrease_health(amount: int) -> void:
+	player_health = mini(player_health - amount, 0)
+	health_changed.emit(player_health)
